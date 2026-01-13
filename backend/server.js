5936140e56156e3c9fd748008f14115a66d82db5
pedrogoliv <pedro.oliveira@ipvc.pt>
2026-01-13 15:52:16 +0000

    tentativa fix render

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+app.use(cors()); // TEMPORARIO
 const mongoose = require("mongoose");
 const http = require("http");
 const { Server } = require("socket.io");
